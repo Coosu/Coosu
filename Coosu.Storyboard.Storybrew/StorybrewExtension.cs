669164e4c802b527dac054b4206f71709c11d9c9
Milkitic <Milkitic@126.com>
2021-08-19 14:00:04 +0800

    add submodules

--- Coosu.Storyboard.Storybrew/StorybrewExtension.cs
+++ Coosu.Storyboard.Storybrew/StorybrewExtension.cs
@@ -9,19 +9,19 @@ namespace Coosu.Storyboard.Storybrew
         public static void ExecuteBrew(this Sprite sprite, StoryboardLayer brewLayer)
         {
             OsbSprite brewObj;
-            if (sprite is Animation animation)
-                brewObj = brewLayer.CreateAnimation(animation.ImagePath,
-                    (int)animation.FrameCount,
-                    (int)animation.FrameDelay,
-                    BrewConvert.CvtLoopType(animation.LoopType),
-                    BrewConvert.CvtOrigin(animation.OriginType),
-                    new Vector2((float)animation.DefaultX,
-                        (float)animation.DefaultY));
-            else
-                brewObj = brewLayer.CreateSprite(sprite.ImagePath,
-                    BrewConvert.CvtOrigin(sprite.OriginType),
-                    new Vector2((float)sprite.DefaultX, (float)sprite.DefaultY)
-                );
+            //if (sprite is Animation animation)
+            //    brewObj = brewLayer.CreateAnimation(animation.ImagePath,
+            //        (int)animation.FrameCount,
+            //        (int)animation.FrameDelay,
+            //        BrewConvert.CvtLoopType(animation.LoopType),
+            //        BrewConvert.CvtOrigin(animation.OriginType),
+            //        new Vector2((float)animation.DefaultX,
+            //            (float)animation.DefaultY));
+            //else
+            //    brewObj = brewLayer.CreateSprite(sprite.ImagePath,
+            //        BrewConvert.CvtOrigin(sprite.OriginType),
+            //        new Vector2((float)sprite.DefaultX, (float)sprite.DefaultY)
+            //    );
         }
 
         public static void ExecuteBrew(this Layer layer, StoryboardLayer brewLayer)
