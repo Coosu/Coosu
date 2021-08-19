14cf71d2f752afd66bd20b5b41e7a5b92cd4fb8d
Milkitic <Milkitic@126.com>
2021-08-19 11:55:16 +0800

    clean submodule first

--- Coosu.Storyboard.Storybrew/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Coosu.Storyboard.Storybrew
-{
-    public class Class1
-    {
-    }
-}
---
+++ Coosu.Storyboard.Storybrew/StorybrewExtension.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Numerics;
+using StorybrewCommon.Storyboarding;
+
+namespace Coosu.Storyboard.Storybrew
+{
+    public static class StorybrewExtension
+    {
+        public static void ExecuteBrew(this Sprite sprite, StoryboardLayer brewLayer)
+        {
+            OsbSprite brewObj;
+            if (sprite is Animation animation)
+                brewObj = brewLayer.CreateAnimation(animation.ImagePath,
+                    (int)animation.FrameCount,
+                    (int)animation.FrameDelay,
+                    BrewConvert.CvtLoopType(animation.LoopType),
+                    BrewConvert.CvtOrigin(animation.OriginType),
+                    new Vector2((float)animation.DefaultX,
+                        (float)animation.DefaultY));
+            else
+                brewObj = brewLayer.CreateSprite(sprite.ImagePath,
+                    BrewConvert.CvtOrigin(sprite.OriginType),
+                    new Vector2((float)sprite.DefaultX, (float)sprite.DefaultY)
+                );
+        }
+
+        public static void ExecuteBrew(this Layer layer, StoryboardLayer brewLayer)
+        {
+            foreach (var sprite in layer.SceneObjects.Where(k => k is Sprite).Cast<Sprite>())
+            {
+                sprite.ExecuteBrew(brewLayer);
+            }
+        }
+    }
+}
