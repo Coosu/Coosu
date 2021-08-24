20f64e7ef20c8806e42ea24143d704cbb87c4b25
Milkitic <Milkitic@126.com>
2021-08-24 16:00:00 +0800

    update script

--- Coosu.Storyboard.Storybrew/CombineXml.ps1
+++ Coosu.Storyboard.Storybrew/CombineXml.ps1
@@ -4,14 +4,20 @@ if ($null -eq $baseDir) {
     $baseDir = "./"
 }
 
-$mainContent = Get-Content -Path "$($baseDir)Coosu.Storyboard.Storybrew.xml"
+try {
+    $mainContent = Get-Content -Path "$($baseDir)\Coosu.Storyboard.Storybrew.xml"
 
-$xdoc = [System.Xml.Linq.XDocument]::Parse($mainContent)
-$members = $xdoc.Element("doc").Element("members")
+    $xdoc = [System.Xml.Linq.XDocument]::Parse($mainContent)
+    $members = $xdoc.Element("doc").Element("members")
 
-Select-Xml -Path "$($baseDir)Coosu.Shared.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
-Select-Xml -Path "$($baseDir)Coosu.Storyboard.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
-Select-Xml -Path "$($baseDir)Coosu.Storyboard.Extensions.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
+    Select-Xml -Path "$($baseDir)\Coosu.Shared.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
+    Select-Xml -Path "$($baseDir)\Coosu.Storyboard.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
+    Select-Xml -Path "$($baseDir)\Coosu.Storyboard.Extensions.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
 
-$str = "<?xml version=""1.0""?>`n$($xdoc.ToString())";
-Write-Output $str >"$($baseDir)Coosu.Storyboard.Storybrew-ILMerged$($append).xml"
+    $str = "<?xml version=""1.0""?>`n$($xdoc.ToString())";
+    Write-Output $str >"$($baseDir)\Coosu.Storyboard.Storybrew-ILMerged$($append).xml"
+    Write-Output "Wrote new document to ""$($baseDir)\Coosu.Storyboard.Storybrew-ILMerged$($append).xml"""
+}
+catch {
+    Write-Error $_.Exception.ToString()
+}
