48d70821defa0ed980aebc251f9adbd208ff8a99
Milkitic <Milkitic@126.com>
2021-09-01 22:55:03 +0800

    fuc..

--- Coosu.Storyboard.Storybrew/CombineXml.ps1
+++ Coosu.Storyboard.Storybrew/CombineXml.ps1
@@ -13,7 +13,6 @@ try {
     Select-Xml -Path "$($baseDir)\Coosu.Shared.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
     Select-Xml -Path "$($baseDir)\Coosu.Storyboard.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
     Select-Xml -Path "$($baseDir)\Coosu.Storyboard.Extensions.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
-    Select-Xml -Path "$($baseDir)\Coosu.Storyboard.Advanced.xml" -XPath '/doc/members/*' | ForEach-Object { $members.Add([System.Xml.Linq.XElement]::Parse($_.Node.OuterXml)) }
 
     $str = "<?xml version=""1.0""?>`n$($xdoc.ToString())";
     Write-Output $str >"$($baseDir)\Coosu.Storyboard.Storybrew-ILMerged$($append).xml"
--- Tests/TextWpfTest/App.xaml.cs
+++ Tests/TextWpfTest/App.xaml.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using Coosu.Storyboard.Advanced.Text;
 using Size = System.Windows.Size;
 
 namespace TextWpfTest
