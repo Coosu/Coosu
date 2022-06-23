2edb19cb1fb34528ee90151a658a95ecbbb0b45b
Milkitic <Milkitic@126.com>
2022-06-23 11:56:06 +0800

    Add DebuggerDisplay

--- Coosu.Beatmap/Extensions/Playback/ControlNode.cs
+++ Coosu.Beatmap/Extensions/Playback/ControlNode.cs
@@ -1,8 +1,19 @@
-﻿namespace Coosu.Beatmap.Extensions.Playback
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Coosu.Beatmap.Extensions.Playback
 {
+    [DebuggerDisplay("{DebuggerDisplay}")]
     public class ControlNode : HitsoundNode
     {
         public SlideChannel SlideChannel { get; internal set; }
         public ControlType ControlType { get; internal set; }
+
+        public string DebuggerDisplay => $"CT{(UseUserSkin ? "D" : "")}:{Offset}: " +
+                                         $"O{Offset}: " +
+                                         $"T{(int)ControlType}{(ControlType is ControlType.StartSliding or ControlType.StopSliding ? (int)SlideChannel : "")}: " +
+                                         $"V{(Volume * 10):#.#}: " +
+                                         $"B{(Balance * 10):#.#}: " +
+                                         $"{(Filename == null ? "" : Path.GetFileNameWithoutExtension(Filename))}";
     }
 }
--- Coosu.Beatmap/Extensions/Playback/PlayableNode.cs
+++ Coosu.Beatmap/Extensions/Playback/PlayableNode.cs
@@ -1,7 +1,17 @@
-﻿namespace Coosu.Beatmap.Extensions.Playback
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Coosu.Beatmap.Extensions.Playback
 {
+    [DebuggerDisplay("{DebuggerDisplay}")]
     public class PlayableNode : HitsoundNode
     {
         public PlayablePriority PlayablePriority { get; set; }
+
+        public string DebuggerDisplay => $"PL{(UseUserSkin ? "D" : "")}:{Offset}: " +
+                                         $"P{(int)PlayablePriority}: " +
+                                         $"V{(Volume * 10):#.#}: " +
+                                         $"B{(Balance * 10):#.#}: " +
+                                         $"{Path.GetFileNameWithoutExtension(Filename)}";
     }
 }
