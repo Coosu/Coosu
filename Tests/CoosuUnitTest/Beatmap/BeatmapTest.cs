832d26e4666e390f91b15ee2aec42bedcc3ad404
Milkitic <Milkitic@126.com>
2022-06-23 10:29:53 +0800

    Drop support for net45

--- Tests/CoosuUnitTest/Beatmap/BeatmapTest.cs
+++ Tests/CoosuUnitTest/Beatmap/BeatmapTest.cs
@@ -1,10 +1,12 @@
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Coosu.Api.HttpClient;
 using Coosu.Api.V2;
 using Coosu.Api.V2.RequestModels;
 using Coosu.Api.V2.ResponseModels;
 using Coosu.Beatmap;
+using Coosu.Beatmap.Extensions.Playback;
 using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -25,6 +27,22 @@ namespace CoosuUnitTest.Beatmap
 
             osuFile.Metadata.Version += " (TEST)";
             osuFile.WriteOsuFile(Path.Combine(folder, osuFile.GetPath(osuFile.Metadata.Version)));
+        }
+
+        [TestMethod]
+        public async Task ReadHitsounds()
+        {
+            var folder = @"F:\milkitic\Songs\1376486 Risshuu feat. Choko - Take";
+            var file = @"Risshuu feat. Choko - Take (yf_bmp) [Ta~ke take take take take take tatata~].osu";
+            var osuDir = new OsuDirectory(folder);
+            await osuDir.InitializeAsync(file);
+
+            var hitsoundList = await osuDir.GetHitsoundNodesAsync(osuDir.OsuFiles[0]);
+            var playableNodes = hitsoundList
+                .OrderBy(k => k.Offset)
+                .Where(k => k is PlayableNode { PlayablePriority: PlayablePriority.Primary })
+                .Cast<PlayableNode>()
+                .ToList();
         }
     }
 }
