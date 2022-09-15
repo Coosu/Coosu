3ff02803243c7beb7d386d243b57f70217e3076a
Milkitic <Milkitic@126.com>
2022-09-15 14:15:03 +0800

    Update StoryboardLogicException.cs

--- Coosu.Storyboard.Storybrew/StoryboardLogicException.cs
+++ Coosu.Storyboard.Storybrew/StoryboardLogicException.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Coosu.Storyboard.Storybrew;
 
 [Serializable]
 public class StoryboardLogicException : Exception
 {
+    public StoryboardLogicException()
+    {
+    }
     public StoryboardLogicException(string? message) : base(message)
+    {
+    }
+    
+    public StoryboardLogicException(string message, Exception innerException) 
+        : base(message, innerException)
+    {
+    }
+
+    protected StoryboardLogicException(SerializationInfo info, StreamingContext context) 
+        : base(info, context)
     {
     }
 }
